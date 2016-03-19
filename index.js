de8bd3405262e374145ddace5f6f5849a58a88a1
Bernhard Widtmann <bernhard.widtmann@maxdome.de>
2016-03-19 15:21:45 +0100

    implement mxd asset API endpoint to heimdall-ext-stage.maxdome.de

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var request =require('superagent');
 
 // Constants
 var PORT = process.env.PORT || 8080;
@@ -8,6 +9,21 @@ var app = express();
 
 app.get('/hello', function (req, res) {
     res.send('hello world!');
+});
+
+app.get('/assets/:id', function (req, res) {
+    request
+        .get('https://heimdall-ext-stage.maxdome.de/api/assets/' + req.params.id)
+        .set('accept-language', 'de_DE')
+        .set('accept', 'application/vnd.maxdome.im.v8+json')
+        .set('clienttype', 'webportal')
+        .set('content-type', 'application/json')
+        .set('platform', 'web')
+        //.set('mxd-session', util.getCookie('mxd-bbe-session')
+        .end(function(err, response){
+                res.send(response);
+            }
+        );
 });
 
 app.listen(PORT);
